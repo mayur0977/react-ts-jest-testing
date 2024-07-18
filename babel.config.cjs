ba54ebd843d13e99ae9a74d5dbfe4913efe59c67
Mayur B Patel <mayur.b.patel@1rivet.com>
2024-07-18 15:40:08 +0530

    Add : basic setup for testing config

---
+++ babel.config.cjs
@@ -0,0 +1,15 @@
+/* eslint-disable no-undef */
+module.exports = {
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          esmodules: true,
+        },
+      },
+    ],
+    ["@babel/preset-react", { runtime: "automatic" }],
+    "@babel/preset-typescript",
+  ],
+};
